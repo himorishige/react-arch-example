c736bbb049b61cae9eb687b482f85ca49dfdd10d
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-03-12 20:27:16 +0900

    add: ColocatePage

---
+++ src/pages/ColocatePage/ColocatePage.tsx
@@ -0,0 +1,9 @@
+import { Colocate } from 'src/components/Colocate';
+
+export const ColocatePage: React.VFC = () => {
+  return (
+    <div className="p-2">
+      <Colocate />
+    </div>
+  );
+};
