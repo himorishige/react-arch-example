6bdb33d7ee89407afecb82446884c0a0d27d5073
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-03-17 09:50:33 +0900

    add: ContainerPresenterSample

---
+++ src/components/ContainerPresenter/ContainerPresenter.tsx
@@ -0,0 +1,54 @@
+import { Avatar, Size } from 'src/components/Avatar';
+import { Spinner } from 'src/components/Spinner';
+
+import { usePosts } from 'src/hooks/usePosts';
+
+import type { Post } from 'src/types';
+
+type Props = {
+  posts?: Post[];
+  isLoading: boolean;
+  size: Size;
+  src: string;
+};
+
+export const Presenter: React.VFC<Props> = (props) => {
+  const { posts, isLoading, size, src } = props;
+
+  if (isLoading)
+    return (
+      <div className="flex justify-center items-center w-screen h-screen">
+        <Spinner size="xl" />
+      </div>
+    );
+
+  return (
+    <div>
+      <div className="mb-2">
+        <Avatar size={size} src={src} />
+      </div>
+      <ul>
+        {posts &&
+          posts.map((post) => <li key={post.id.toString()}>{post.title}</li>)}
+      </ul>
+    </div>
+  );
+};
+
+export const Container: React.VFC = () => {
+  const { usePostsQuery } = usePosts();
+
+  const { data: posts, isLoading } = usePostsQuery({
+    deps: ['container'],
+    params: { _limit: 5 },
+  });
+
+  return (
+    <Presenter
+      posts={posts}
+      isLoading={isLoading}
+      size="large"
+      src="https://avatars.dicebear.com/v2/male/e828b4072fdb3dc6312b67977f0b247a.svg"
+    />
+  );
+};
