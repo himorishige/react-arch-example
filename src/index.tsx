ca8fe1be1318c64fc68d9501f79b5a3dc28f272b
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-04-02 09:47:26 +0900

    fix: for react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from 'src/App';
 import 'regenerator-runtime';
 import 'tailwindcss/tailwind.css';
@@ -7,9 +7,12 @@ import { initMocks } from 'src/test/server';
 
 initMocks();
 
-render(
+const container = document.getElementById('root');
+if (!container) throw new Error('Failed to find the root element');
+const root = createRoot(container);
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root'),
 );
