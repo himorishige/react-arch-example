ab09d80f824bd5bb28da69186fe21f66456fe1be
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-04-02 17:31:40 +0900

    update: suspense example

---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,27 @@
+import { Avatar } from 'src/components/Avatar';
+
+import { usePosts } from 'src/hooks/usePosts';
+
+export const Posts: React.VFC = () => {
+  const { usePostsQuery } = usePosts();
+
+  const { data: posts } = usePostsQuery({
+    deps: ['suspense'],
+    params: { _limit: 5 },
+  });
+
+  return (
+    <div>
+      <div className="mb-2">
+        <Avatar
+          size="large"
+          src="https://avatars.dicebear.com/v2/male/e828b4072fdb3dc6312b67977f0b247a.svg"
+        />
+      </div>
+      <ul>
+        {posts &&
+          posts.map((post) => <li key={post.id.toString()}>{post.title}</li>)}
+      </ul>
+    </div>
+  );
+};
