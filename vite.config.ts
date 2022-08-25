917a15b73771c946b8de318a5562748116baa0dc
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-08-25 10:39:20 +0900

    update: config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import env from 'vite-plugin-env-compatible';
 import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
   plugins: [
@@ -19,5 +21,16 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './.vitest/setup.ts',
+    css: true,
+    testTimeout: 60_000,
+    hookTimeout: 60_000,
+    exclude: [...configDefaults.exclude, 'e2e/*'],
+    coverage: {
+      reporter: ['text', 'json-summary', 'json'],
+      lines: 60,
+      branches: 60,
+      functions: 60,
+      statements: 60,
+    },
   },
 });
