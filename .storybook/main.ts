7d766da34ac0bd3e6618b7eb8d4835dd85bf0f98
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2023-01-02 12:06:05 +0900

    update: sotrybook 7 beta

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,57 +1,40 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import env from 'vite-plugin-env-compatible';
-import svgrPlugin from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
-
 const config = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-a11y',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-vite',
-  },
-  features: {
-    storyStoreV7: true,
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss')
+      }
+    }
+  }],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
   },
   async viteFinal(config) {
-    const plugins = config.plugins.filter((plugin) => {
-      return !(
-        Array.isArray(plugin) && plugin[0]?.name.startsWith('vite:react')
-      );
+    const plugins = config.plugins.filter(plugin => {
+      return !(Array.isArray(plugin) && plugin[0]?.name.startsWith('vite:react'));
     });
-
     return {
       ...config,
-      plugins: [
-        ...plugins,
-        react(),
-        svgrPlugin(),
-        tsconfigPaths(),
-        env({ prefix: 'REACT_APP_' }),
-      ],
+      plugins: [...plugins, react(), tsconfigPaths(), env({
+        prefix: 'REACT_APP_'
+      })],
       resolve: {
-        alias: [
-          {
-            find: 'src',
-            replacement: path.resolve(__dirname, '../src'),
-          },
-        ],
-      },
+        alias: [{
+          find: 'src',
+          replacement: path.resolve(__dirname, '../src')
+        }]
+      }
     };
   },
+  docs: {
+    autodocs: true
+  }
 };
-
 export default config;
